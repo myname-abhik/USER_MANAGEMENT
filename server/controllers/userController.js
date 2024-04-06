0fffbecb574303bdefb7dcdba2e176bb76199282
myname-abhik <abhik16chakrabortty@gmail.com>
2024-04-07 01:40:21 +0530

    try to use _dirname

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,7 +15,7 @@ exports.view = (req, res) => {
     // When done with the connection, release it
     if (!err) {
       let removedUser = req.query.removed;
-      res.render('home', { rows, removedUser });
+      res.render(__dirname +'./views/home.hbs', { rows, removedUser });
     } else {
       console.log(err);
     }
