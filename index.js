e5ed1ae332ac5eeb46dbdf9445d5bf3e170ea424
myname-abhik <abhik16chakrabortty@gmail.com>
2024-04-06 23:10:59 +0530

    check for error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 require('dotenv').config();
 
-const port=process.env.PORT || 5000;
+const port = process.env.PORT||5000;
 const bodyparser = require('body-parser');
 const mysql = require('mysql');
 
