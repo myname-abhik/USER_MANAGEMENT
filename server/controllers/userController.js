c930eb817f02c64e0fbabb6cda049d2dcc6fa733
myname-abhik <abhik16chakrabortty@gmail.com>
2024-04-07 17:03:13 +0530

    16

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,7 +15,7 @@ exports.view = (req, res) => {
     // When done with the connection, release it
     if (!err) {
       // let removedUser = req.query.removed;
-      res.render('home.hbs', {rows});
+      res.render('./partials/home', {rows});
       // res.json({start :"hello"});
     } else {
       console.log(err);
